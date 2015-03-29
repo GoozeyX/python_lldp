df031bc953a871d02e92935889db5ff7264218bf
Alexander Brovman <alexander.brovman@gmail.com>
2015-03-30 01:56:58 +0200

    prechange

--- alpha0.0.1.py
+++ alpha0.0.1.py
@@ -17,10 +17,28 @@ while True:
 
     if ethernetHeaderProtocol != '\x88\xCC':
         continue
+    #[0] at the end of the unpack is because of the tuple returnvalue
+    tlv_header = struct.unpack("!H", lldpPayload[:2])[0]
+    tlv_type = (tlv_header & 0xfe00) >> 9
+    tlv_len = (tlv_header & 0x01ff)
+    tlv_payload = lldpPayload[2:tlv_len + 2]
+    # print tlvtype
+    # print tlvdata
+    tlv_oui = None
+    tlv_subtype = None
+    if tlv_type == 0x7f:
+        _tlv_oui = unpack("!BBB", tlv_payload[:3])
+        tlv_subtype = unpack("!B", tlv_payload[3:3 + 1])[0]
+        tlv_payload = tlv_payload[3 + 1:]
 
-    lldp_tlv_header = struct.unpack("!H", lldpPayload[:2])
-    print lldp_tlv_header
-    print binascii.hexlify(lldp_tlv_header)
+    print _tlv_oui
+    print tlv_type
+    print tlv_subtype
+    print tlv_payload
+
+
+    # print lldp_tlv_header
+    # print binascii.hexlify(lldp_tlv_header)
 
     print "****************_ETHERNET_FRAME_****************"
     print "Type:            ", binascii.hexlify(ethernetHeaderProtocol)
@@ -34,3 +52,34 @@ while True:
 # buff = buff[tlvlen + 2:]
 # lldp_info.append((tlvtype, tlvdata))
 # return lldp_info
+
+# # LLDP Length:
+# LLDP_TLV_TYPE_BIT_LEN = 7
+# LLDP_TLV_LEN_BIT_LEN = 9
+# LLDP_TLV_HEADER_LEN = 2         # 7 + 9 = 16
+# LLDP_TLV_OUI_LEN = 3
+# LLDP_TLV_SUBTYPE_LEN = 1
+# # LLDP Protocol BitFiddling Mask:
+# LLDP_TLV_TYPE_MASK = 0xfe00
+# LLDP_TLV_LEN_MASK = 0x1ff
+# # LLDP Protocol ID:
+# LLDP_PROTO_ID = 0x88cc
+# # LLDP TLV Type:
+# LLDP_TLV_TYPE_CHASSISID = 0x01
+# LLDP_TLV_TYPE_PORTID = 0x02
+# LLDP_TLV_DEVICE_NAME = 0x05
+# LLDP_PDUEND = 0x00
+# LLDP_TLV_ORGANIZATIONALLY_SPECIFIC = 0x7f
+# # LLDP TLV OUI Type:
+# LLDP_TLV_OUI_802_1 = 0x0008c2
+# LLDP_TLV_OUI_802_3 = 0x00120f
+
+# ## Magic string for unpack packet:
+# UNPACK_ETH_HEADER_DEST = '!%s' % ('B' * ETH_ALEN)
+# UNPACK_ETH_HEADER_SRC = '!%s' % ('B' * ETH_ALEN)
+# UNPACK_ETH_HEADER_PROTO = '!H'
+
+# ## Magic string for unpack LLDP packet:
+# UNPACK_LLDP_TLV_TYPE = '!H'
+# UNPACK_LLDP_TLV_OUI = '!%s' % ('B' * LLDP_TLV_OUI_LEN)
+# UNPACK_LLDP_TLV_SUBTYPE = '!B'
